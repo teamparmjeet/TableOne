3fa8bb53349d830eb3512ed10c7cf0660e2637ce
teamparmjeet <teamparmjeet@gmail.com>
2026-07-09 13:55:58 +0530

    qweqweqwe

--- src/components/TableOneSection.tsx
+++ src/components/TableOneSection.tsx
@@ -164,7 +164,7 @@ export default function TableOneSection() {
           {/* Bottom Text */}
           <p
             className="
-            Note1
+            Note2
             uppercase
             leading-tight
             text-[#B54323] 
